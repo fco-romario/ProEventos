d3f26a1f74e0d1501a4236520253c1bd3d470638
fco-romario <fco.romario.alves@gmail.com>
2023-07-29 00:11:15 -0300

    adicionando font e bootstrap

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -17,6 +18,7 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
